7cecc0cadbecbb64aa3d94e56ca35163dbf6dc3f
Unknown <alexandra.girsberger@edu.tbz.ch>
2019-01-22 08:30:56 +0100

    ChatService & DateService

---
+++ src/ch/tbz/DateService/DateServer.java
@@ -0,0 +1,21 @@
+package ch.tbz.DateService;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.Date;
+
+public class DateServer {
+    public static void main(String[] args) throws IOException {
+    	// Creates a new Socket
+        try (var listener = new ServerSocket(9090)) {
+            System.out.println("The Date Server is running");
+            while (true) {
+                try (var socket = listener.accept()) {
+                    var out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                }
+            }
+        }
+    }
+}
